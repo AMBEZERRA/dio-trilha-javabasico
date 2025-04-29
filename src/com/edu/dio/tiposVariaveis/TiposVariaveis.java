3a8f03df77859820ba5590572f49b190151c5501
Allisson Bezerra <allisson.geografia@gmail.com>
2025-04-28 21:32:38 -0300

    Variáveis na teoria

--- src/com/edu/dio/tiposVariaveis/TiposVariaveis.java
+++ src/com/edu/dio/tiposVariaveis/TiposVariaveis.java
@@ -3,8 +3,20 @@ package com.edu.dio.tiposVariaveis;
 public class TiposVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 8 Tipos primitivos (não são objetos)
+		/*Int
+		 * byte
+		 * short
+		 * long
+		 * double
+		 * boolean
+		 * char
+		 * São Valores Brutos
+		 */
+   // constante
+		final int NUMERO = 5;
+ 
+		System.out.println(NUMERO);
 	}
 
 }
